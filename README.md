e392e348c0865300c9661c28488f96c4cea72772
齐国栋 <guodong_qi@zju.edu.cn>
2019-01-21 19:10:01 +0800

    add 4

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 [1_two_sum.py](./codes/1_two_sum.py) 创建一个字典，判断字典里是否含有target-nums[i]  
 [2_add_two_nums.py](./codes/2_add_two_nums.py) 主要是对迭代器的考察  
 [3_Longest_Substring_Without_Repeating_Characters.py](./codes/3_Longest_Substring_Without_Repeating_Characters.py) 定义滑动窗口，窗口内不需要再过一遍  
-[4_Median_of_Two_Sorted_Arrays.py](./codes/4_Median_of_Two_Sorted_Arrays.py) 类似分治法对两个序列进行排序
+[4_Median_of_Two_Sorted_Arrays.py](./codes/4_Median_of_Two_Sorted_Arrays.py) 类似分治法对两个序列进行排序  
+[5_Longest_Palindromic_Substring.py](./codes/5_Longest_Palindromic_Substring.py) 
+  * 方法1：暴力循环，时间复杂度高 n^3
+  * 方法2：反转序列，寻找s与s’相同的最大的序列组（暂未实现）
+  * 方法3：动态规划，对方法1的改进，避免重复计算已经验证过的回文序列
---
+++ codes/5_Longest_Palindromic_Substring.py
@@ -0,0 +1,64 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        start = 0
+        if len(s) == 0:
+            return ""
+        if len(s) == 1:
+            return s
+        longest = s[0]
+        while start < len(s) - 1:
+            end = start + 1
+            while end < len(s):
+                flag = True
+                for i in range((end - start + 1) // 2):
+                    if s[i + start] != s[end - i]:
+                        flag = False
+                if flag and (end - start) >= len(longest):
+                    longest = s[start:end + 1]
+                end += 1
+            start += 1
+        return longest
+
+    def longestPalindrome3(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        longest = 0
+        # 使用两个参数来表示当前状态，起始索引和末尾索引
+        m = len(s)
+        if not m:
+            return ""
+        if m == 1:
+            return s
+        state = list([[0] * m for row in range(m)])
+
+        def inits():
+            state[0][0] = True
+            for i in range(1, m):
+                state[i][i] = True
+                state[i - 1][i] = s[i] == s[i - 1]
+                state[i][i - 1] = s[i] == s[i - 1]
+            for i in range(1,m-1):
+                state[0][i] = state[0][i + 1] and s[0] == s[i]
+
+        inits()
+        for i in range(0, m - 1):
+            for j in range(i, m):
+                state[i][j] = state[i + 1][j - 1] and s[i] == s[j]
+
+        for i in range(m):
+            for j in range(i, m):
+                if state[i][j] and j - i + 1 > longest:
+                    longest = j - i
+                    longest_s = s[i:j + 1]
+
+        return longest_s
+
+
+s = Solution()
+print(s.longestPalindrome3("abcba"))
