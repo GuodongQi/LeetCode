19fcd03927a3a40b40dd4b735fe00ede7e7e357a
齐国栋 <guodong_qi@zju.edu.cn>
2019-01-21 12:58:50 +0800

    add 4

--- codes/4_Median_of_Two_Sorted_Arrays.py
+++ codes/4_Median_of_Two_Sorted_Arrays.py
@@ -9,24 +9,27 @@ class Solution:
         new_list = []
         i = 0
         j = 0
-        while i < len(nums1) and j < len(nums2):
+        m = len(nums1)
+        n = len(nums2)
+        l = m + n
+        while i < m and j < n:
             if nums1[i] <= nums2[j]:
                 new_list.append(nums1[i])
                 i += 1
             else:
                 new_list.append(nums2[j])
                 j += 1
-        while i < len(nums1):
+        while i < m:
             new_list.append(nums1[i])
             i += 1
-        while j < len(nums2):
+        while j < n:
             new_list.append(nums2[j])
             j += 1
         if len(new_list) % 2:
-            return new_list[int((len(new_list) - 1) / 2)]
+            return new_list[l // 2]
         else:
-            return (new_list[int((len(new_list)) / 2)-1] + new_list[int((len(new_list)) / 2) ]) / 2
+            return (new_list[l // 2 - 1] + new_list[l // 2]) / 2
 
 
 s = Solution()
-print(s.findMedianSortedArrays([], [2,3]))
+print(s.findMedianSortedArrays([], [2, 3]))
