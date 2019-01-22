f6143d7d748cd1aa8c4b27378ebf26ad2d41df0f
齐国栋 <guodong_qi@zju.edu.cn>
2019-01-22 11:21:53 +0800

    update 6

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # LeetCode
-[1_two_sum.py](./codes/1_two_sum.py) 创建一个字典，判断字典里是否含有target-nums[i]  
-[2_add_two_nums.py](./codes/2_add_two_nums.py) 主要是对迭代器的考察  
-[3_Longest_Substring_Without_Repeating_Characters.py](./codes/3_Longest_Substring_Without_Repeating_Characters.py) 定义滑动窗口，窗口内不需要再过一遍  
-[4_Median_of_Two_Sorted_Arrays.py](./codes/4_Median_of_Two_Sorted_Arrays.py) 类似分治法对两个序列进行排序  
-[5_Longest_Palindromic_Substring.py](./codes/5_Longest_Palindromic_Substring.py) 
-  * 方法1：暴力循环，时间复杂度高 n^3
-  * 方法2：反转序列，寻找s与s’相同的最大的序列组（暂未实现）
-  * 方法3：动态规划，对方法1的改进，避免重复计算已经验证过的回文序列
+1. [two_sum.py](./codes/1_two_sum.py)  
+   * 创建一个字典，判断字典里是否含有target-nums[i]  
+2. [add_two_nums.py](./codes/2_add_two_nums.py)  
+   * 主要是对迭代器的考察  
+3. [Longest_Substring_Without_Repeating_Characters.py](./codes/3_Longest_Substring_Without_Repeating_Characters.py)  
+   * 定义滑动窗口，窗口内不需要再过一遍  
+4. [Median_of_Two_Sorted_Arrays.py](./codes/4_Median_of_Two_Sorted_Arrays.py)  
+   * 类似分治法对两个序列进行排序  
+5. [Longest_Palindromic_Substring.py](./codes/5_Longest_Palindromic_Substring.py) 
+   * 方法1：暴力循环，时间复杂度高 n^3
+   * 方法2：反转序列，寻找s与s’相同的最大的序列组（暂未实现）
+   * 方法3：动态规划，对方法1的改进，避免重复计算已经验证过的回文序列
+6. [ZigZag_Conversion.py](./codes/6_ZigZag_Conversion.py)
+   * 方法1：构建形如解释原因的矩阵，方法较慢
+   * 方法2: 建立索引，对每个v字符的位置进行预先定义
+7. 
--- codes/6_ZigZag_Conversion.py
+++ codes/6_ZigZag_Conversion.py
@@ -5,18 +5,64 @@ class Solution:
         :type numRows: int
         :rtype: str
         """
-        m = len(s)
-        v = 2 * numRows - 1
-        v_nums = m // v + 1
-        cols = v_nums * (numRows - 1)
-        # matrix = [[0] * cols for row in range(numRows)]
-        matrix = []
-        v_num = 0
+        # if not len(s):
+        #     return s
 
-        for i in range(m):
-            v_num = i // v
-            bias = i % v
-            matrix.append()
+        m = len(s)  # 字符串长度
+        if numRows == 1 or numRows >=m:
+            return s
+        v = 2 * numRows - 2  # 一个v字结构有多少个字符
+        v_nums = m // v + 1  # 总共有多少个v结构
+        cols = v_nums * (numRows - 1)  # 计算列数
+        matrix = list([[0] * cols for row in range(numRows)])  # 构建矩阵
+        k = 0
+        new_str = []
+        for i in range(v_nums):  # 第i个v结构
+            for j in range(numRows):  # 第i个v结构的第j行
+                if k == m:
+                    break
+                matrix[j][i * (numRows - 1)] = s[k]
+                k += 1
+            for j in range(1, numRows-1):
+                if k == m:
+                    break
+                matrix[numRows - j - 1][i * (numRows - 1) + j] = s[k]
+                k += 1
+
+        for ii in range(numRows):
+            for jj in range(cols):
+                if matrix[ii][jj]:
+                    new_str.append(matrix[ii][jj])
+        return "".join(new_str)
+
+    def convert2(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        # 暴力循环
+        # if not len(s):
+        #     return s
+        if numRows == 1 or numRows >= len(s):
+            return s
+        m = len(s)  # 字符串长度
+        v = 2 * numRows - 2  # 一个v字结构有多少个字符
+        v_nums = m // v + 1  # 总共有多少个v结构
+        rn = list(range(0, numRows - 1)) + list(range(numRows - 1, 0, -1))
+        z = [""] * numRows
+        cols = v_nums * (numRows - 1)  # 计算列数
+        # matrix = list([[0] * cols for row in range(numRows)])  # 构建矩阵
+        k = 0
+        new_str = []
+        for i in range(v_nums):  # 第i个v结构
+            for j in range(v):  # 第i个v结构的第j个字符
+                z[rn[j]] += s[k]
+                if k == m-1:
+                    return "".join(z)
+                else:
+                    k += 1
+
 
 s = Solution()
-s.convert("PAYPALISHIRING", 4)
+print(s.convert2("PAYPALISHIRING", 3))
