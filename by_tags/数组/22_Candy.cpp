def073d477c74a772a55853db518ad5e33f13c58
GuodongQi <guodong_qi@zju.edu.cn>
2019-06-24 11:11:20 +0800

    数组

---
+++ by_tags/数组/22_Candy.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> left2right(ratings.size(),1);
+        vector<int> right2left(ratings.size(),1);
+        int sum = 0;
+        for(int i = 1; i < ratings.size(); i++)
+            if(ratings[i] > ratings[i-1])
+                left2right[i] = left2right[i-1] + 1;
+        for(int i = ratings.size() - 2; i>=0; i--)
+            if(ratings[i] > ratings[i+1])
+                right2left[i] = right2left[i+1] + 1;
+
+        for(int i = 0; i < ratings.size(); i++)
+            sum += right2left[i] > left2right[i] ? right2left[i]:left2right[i];
+
+       return sum;
+    }
+};
