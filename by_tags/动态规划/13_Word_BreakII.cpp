e1cec5d27c20e569c345527f01e89bbcab58c90f
GuodongQi <guodong_qi@zju.edu.cn>
2019-07-12 09:58:15 +0800

    add 动态规划

--- by_tags/动态规划/13_Word_BreakII.cpp
+++ by_tags/动态规划/13_Word_BreakII.cpp
@@ -8,7 +8,7 @@ public:
         f[0] = true;
         for(int  i = 1; i <= s.length();++i){
             for(int j = i-1; j>=0; --j){
-                if(f[i] &&dict.find(s.substr(j, i-j))!=dict.end()){
+                if(f[j] &&dict.find(s.substr(j, i-j))!=dict.end()){
                     f[i] = true;
                     prev[i][j] = true;
                 }
